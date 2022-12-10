6e3b47877692080aed2961d341eff5552de4b1c8
30Irina <93494737+30Irina@users.noreply.github.com>
2022-12-10 18:01:06 +0300

    task3

---
+++ Task3/Infrastructure/ImageConverter.cs
@@ -0,0 +1,51 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Globalization;
+using System.IO;
+using System.Text;
+using System.Windows.Data;
+using System.Windows.Forms;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+
+namespace Task3.Infrastructure
+{
+    internal class ImageConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var imagePaths = value as string[];
+            if (imagePaths is not null)
+            {
+
+                List<BitmapImage> images = new List<BitmapImage>();
+
+                foreach (var imagePath in imagePaths)
+                {
+                    var image = Image.FromFile(imagePath);
+
+                    var bitmap = new BitmapImage();
+
+                    bitmap.BeginInit();
+                    MemoryStream memoryStream = new MemoryStream();
+                    image.Save(memoryStream, ImageFormat.Png);
+                    memoryStream.Seek(0, SeekOrigin.Begin);
+                    bitmap.StreamSource = memoryStream;
+                    bitmap.EndInit();
+
+                    images.Add(bitmap);
+                }
+
+                return images;
+            }
+
+            return null;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) => throw new NotImplementedException();
+    }
+}
---
+++ Task3/Migrations/20221210133051_InitialCreate.cs
@@ -0,0 +1,87 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Task3.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Images",
+                columns: table => new
+                {
+                    ImageId = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Name = table.Column<string>(type: "TEXT", nullable: false),
+                    Hashcode = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Images", x => x.ImageId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "bytes",
+                columns: table => new
+                {
+                    ImageId = table.Column<int>(type: "INTEGER", nullable: false),
+                    BLOB = table.Column<byte[]>(type: "BLOB", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_bytes", x => x.ImageId);
+                    table.ForeignKey(
+                        name: "FK_bytes_Images_ImageId",
+                        column: x => x.ImageId,
+                        principalTable: "Images",
+                        principalColumn: "ImageId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Embeddings",
+                columns: table => new
+                {
+                    EmbeddingId = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    distance = table.Column<float>(type: "REAL", nullable: false),
+                    similarity = table.Column<float>(type: "REAL", nullable: false),
+                    PairImage1 = table.Column<int>(type: "INTEGER", nullable: false),
+                    PairImage2 = table.Column<int>(type: "INTEGER", nullable: false),
+                    ImageId = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Embeddings", x => x.EmbeddingId);
+                    table.ForeignKey(
+                        name: "FK_Embeddings_Images_ImageId",
+                        column: x => x.ImageId,
+                        principalTable: "Images",
+                        principalColumn: "ImageId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Embeddings_ImageId",
+                table: "Embeddings",
+                column: "ImageId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "bytes");
+
+            migrationBuilder.DropTable(
+                name: "Embeddings");
+
+            migrationBuilder.DropTable(
+                name: "Images");
+        }
+    }
+}
